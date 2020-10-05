f6f9dba2c3e394785c9d36a1583ff3d8e7d9abe6
krishnab <cyclotomiq@gmail.com>
2020-10-05 13:39:44 -0400

    updated test cases with compression test for large file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ import requests
 
 collect_ignore = ['setup.py']
 
+
 @pytest.fixture(scope="function")
 def small_image(tmpdir):
     url = 'https://github.com/00krishna-tools/nyuki/releases/download/v0.0.1/sample_image_small.tif'
@@ -17,6 +18,18 @@ def small_image(tmpdir):
         f.write(r.content)
 
     return os.path.join(str(tmpdir), 'sample_image_small.tif')
+
+
+@pytest.fixture(scope="function")
+def big_image(tmpdir):
+    url = 'https://github.com/00krishna-tools/nyuki/releases/download/v0.0.1/sample_image_big.tif'
+
+    r = requests.get(url)
+
+    with open(os.path.join(str(tmpdir), 'sample_image_big.tif'), 'wb') as f:
+        f.write(r.content)
+
+    return os.path.join(str(tmpdir), 'sample_image_big.tif')
 
 
 @pytest.fixture(scope="function")
--- tests/test_nyuki_compress.py
+++ tests/test_nyuki_compress.py
@@ -8,13 +8,27 @@ import nyuki
 from nyuki.geotiff_compressor import compressor
 import os
 
+
 @pytest.mark.usefixtures('small_image')
+@pytest.mark.usefixtures('big_image')
 
 
-@pytest.mark.parametrize("compression", ['LZW','JPEG', 'JPEG2000','DEFLATE', 'NONE'])
-def test_lzw_compression(small_image, compression):
-
+@pytest.mark.parametrize("compression", ['LZW',
+                                         'JPEG',
+                                         'JPEG2000',
+                                         'DEFLATE',
+                                         'NONE'])
+def test_small_compression(small_image, compression):
     newfile = compressor(small_image, compression, yes=True)
     res = nyuki.utilities.dhash_distance(small_image, newfile, hash_size=8)
+    os.remove(newfile)
+    assert res < 0.20
+
+
+@pytest.mark.parametrize("compression", ['JPEG'])
+def test_bigtiff_compression(big_image, compression):
+
+    newfile = compressor(big_image, compression, yes=True)
+    res = nyuki.utilities.dhash_distance(big_image, newfile, hash_size=8)
     os.remove(newfile)
     assert res < 0.20
